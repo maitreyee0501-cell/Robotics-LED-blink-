6d0ff89a17d3e9dcb9fa1169a0ee66f00d255ec3
maitreyee0501-cell <maitreyee0501@gmail.com>
2026-01-30 12:24:18 +0530

    Add files via upload

---
+++ Button_blink.ino
@@ -0,0 +1,25 @@
+// set pin numbers:
+const int buttonPin = 2;  // number of button pin
+const int ledPin = 13;    // number of the LED pin
+int buttonState = 0;  // variable for reading the pushbutton status
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+    delay(100);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
